65130ea40c695d3686b271a5d720a450902bde36
Geran <securiteg2a@gmail.com>
2022-06-08 17:24:58 +0200

    first commit

---
+++ src/module-info.java
@@ -0,0 +1,16 @@
+module MuseoOriginal {
+	exports com.raven.event;
+	exports com.raven.main;
+	exports com.raven.model;
+	exports com.raven.swing.icon;
+	exports com.raven.component;
+	exports com.raven.swing;
+	exports com.raven.swing.table;
+	exports com.raven.dialog;
+	exports com.raven.form;
+	exports com.raven.swing.scrollbar;
+
+	requires TimingFramework;
+	requires java.desktop;
+	requires java.logging;
+}
